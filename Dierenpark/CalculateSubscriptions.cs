1bbb7f8eacef9e7e174b0b44782103f44cc7b2d7
Vincent Theune <vincent.theune@upcmail.nl>
2020-08-14 18:11:54 +0200

    Add project files.

---
+++ Dierenpark/CalculateSubscriptions.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dierenpark
+{
+    class CalculateSubscriptions
+    {
+        private const int adultArrayPosition = 0;
+        private const int childArrayPosition = 1;
+        private const int elderlyArrayPosition = 2;
+
+        private const int arraySize = 3;
+
+        private const int minAdultAge = 18;
+        private const int minElderlyAge = 65;
+
+        private const string nameCouple = "Married couple";
+        private const string nameFamilyOneChild = "Gezin met 1 kind";
+        private const string nameFamilyTwoChilderen = "Gezin met 2 kinderen";
+        private const string namePerson = "Person";
+        private const string nameExtraChild = "an extra child";
+        private const string nameElderlyCouple = "Couple 65+";
+        private const string nameElderlyPerson = "Person 65+";
+
+        private static readonly int[] qualifierCouple = { 2, 0, 0 };
+        private static readonly int[] qualifierFamilyOneChild = { 2, 1, 0 };
+        private static readonly int[] qualifierFamilyTwoChilderen = { 2, 2, 0 };
+        private static readonly int[] qualifierPerson = { 1, 0, 0 };
+        private static readonly int[] qualifierExtraChild = { 0, 1, 0 };
+        private static readonly int[] qualifierElderlyCouple = { 0, 0, 2 };
+        private static readonly int[] qualifierElderlyPerson = { 0, 0, 1 };
+
+        private const int priceCouple = 61;
+        private const int priceFamilyOneChild = 71;
+        private const int priceFamilyTwoChilderen = 82;
+        private const int pricePerson = 30;
+        private const int priceExtraChild = 11;
+        private const int priceElderlyCouple = 65;
+        private const int priceElderlyPerson = 26;
+
+        private const string stringBreak = "\r\n";
+
+        private static List<SubscriptionOption> subscriptionOptions;
+
+        public static int GetArraySize()
+        {
+            return arraySize;
+        }
+        public static int GetAdultArrayPosition()
+        {
+            return adultArrayPosition;
+        }
+        public static int GetChildArrayPosition()
+        {
+            return childArrayPosition;
+        }
+        public static int GetElderlyArrayPosition()
+        {
+            return elderlyArrayPosition;
+        }
+        public static int GetMinAdultAge()
+        {
+            return minAdultAge;
+        }
+        public static int GetMinElderlyAge()
+        {
+            return minElderlyAge;
+        }
+
+        public static string GetSubscriptionsString(int[] peopleCount)
+        {
+            SubscriptionOptionsExists();
+            List<SubscriptionOption> subscriptions = GetSubscriptionOptionsOfPeopleCount(peopleCount, new List<SubscriptionOption>(), null);
+            string toReturn = "";
+            foreach(SubscriptionOption subscription in subscriptions)
+            {
+                toReturn += subscription.GetName() + stringBreak;
+            }
+            toReturn += GetSubscriptionOptionsTotalPrice(subscriptions);
+            return toReturn;
+        }
+        private static List<SubscriptionOption> GetSubscriptionOptionsOfPeopleCount (int[] peopleCount, List<SubscriptionOption> workingOnOptions, List<SubscriptionOption> workingOptions)
+        {
+            foreach(SubscriptionOption subscriptionOption in subscriptionOptions)
+            {
+                if (subscriptionOption.CanApply(peopleCount))
+                {
+                    List<SubscriptionOption> newSubscriptionOptions = CopySubscriptionOptions(workingOnOptions);
+                    newSubscriptionOptions.Add(subscriptionOption);
+
+                    if (workingOptions == null || GetSubscriptionOptionsTotalPrice(newSubscriptionOptions) < GetSubscriptionOptionsTotalPrice(workingOptions))
+                    {
+
+                        int[] newPeopleCount = subscriptionOption.Apply(peopleCount);
+
+                        bool noPeopleLeft = true;
+                        foreach (int count in newPeopleCount)
+                        {
+                            if (count != 0)
+                            {
+                                noPeopleLeft = false;
+                                break;
+                            }
+                        }
+                        if (noPeopleLeft)
+                        {
+                            workingOptions = newSubscriptionOptions;
+                        }
+                        else
+                        {
+                            workingOptions = GetSubscriptionOptionsOfPeopleCount(newPeopleCount, newSubscriptionOptions, workingOptions);
+                        }
+                    }
+                }
+            }
+            return workingOptions;
+        }
+
+        private static int GetSubscriptionOptionsTotalPrice(List<SubscriptionOption> subscriptionOptions)
+        {
+            int total = 0;
+            foreach(SubscriptionOption option in subscriptionOptions)
+            {
+                total += option.GetPrice();
+            }
+            return total;
+        }
+        private static List<SubscriptionOption> CopySubscriptionOptions(List<SubscriptionOption> toCopy)
+        {
+            List<SubscriptionOption> toReturn = new List<SubscriptionOption>();
+            foreach(SubscriptionOption option in toCopy)
+            {
+                toReturn.Add(option);
+            }
+            return toReturn;
+        }
+
+        private static void SubscriptionOptionsExists()
+        {
+            if(subscriptionOptions == null)
+            {
+                subscriptionOptions = new List<SubscriptionOption>();
+
+                subscriptionOptions.Add(new SubscriptionOption(nameCouple, qualifierCouple, priceCouple));
+                subscriptionOptions.Add(new SubscriptionOption(nameFamilyOneChild, qualifierFamilyOneChild, priceFamilyOneChild));
+                subscriptionOptions.Add(new SubscriptionOption(nameFamilyTwoChilderen, qualifierFamilyTwoChilderen, priceFamilyTwoChilderen));
+                subscriptionOptions.Add(new SubscriptionOption(namePerson, qualifierPerson, pricePerson));
+                subscriptionOptions.Add(new SubscriptionOption(nameExtraChild, qualifierExtraChild, priceExtraChild));
+                subscriptionOptions.Add(new SubscriptionOption(nameElderlyCouple, qualifierElderlyCouple, priceElderlyCouple));
+                subscriptionOptions.Add(new SubscriptionOption(nameElderlyPerson, qualifierElderlyPerson, priceElderlyPerson));
+            }
+        }
+
+        private class SubscriptionOption
+        {
+            private const string notSameLenthExeptionText = "array given to SubscriptionOption isn't the same size as it's qualifier";
+
+            private string name;
+            private int[] qualifier;
+            private int price;
+
+            internal SubscriptionOption(string name, int[] qualifier, int price)
+            {
+                this.name = name;
+                this.qualifier = qualifier;
+                this.price = price;
+            }
+            internal bool CanApply(int[] peopleCount)
+            {
+                if (peopleCount.Length == qualifier.Length)
+                {
+                    for (int i = 0; i < qualifier.Length; i++)
+                    {
+                        if (peopleCount[i] < qualifier[i])
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                throw new Exception(notSameLenthExeptionText);
+            }
+            internal int[] Apply(int[] peopleCount)
+            {
+                if (peopleCount.Length == qualifier.Length)
+                {
+                    int[] toReturn = new int[qualifier.Length];
+                    for (int i = 0; i < qualifier.Length; i++)
+                    {
+                        toReturn[i] = peopleCount[i] - qualifier[i];
+                    }
+                    return toReturn;
+                }
+                throw new Exception(notSameLenthExeptionText);
+            }
+            internal string GetName()
+            {
+                return name;
+            }
+            internal int GetPrice()
+            {
+                return price;
+            }
+        }
+    }
+}
---
+++ Dierenpark/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dierenpark
+{
+    public partial class Form1 : Form
+    {
+        private const string currentDateLabelText = "current date: ";
+        private const string addBirthdateButtonText = "add a person";
+        private const string calculateSubscriptionButtonText = "calculate Subcription price";
+
+        private const int widthMargin = 10;
+        private const int heightMargin = 10;
+        private const int rowHeight = 30;
+
+        private const int addBirthdateButtonWidth = 100;
+        private const int calculateSubscriptionButtonWidth = 150;
+
+        private Date currentDate;
+        private List<BirthDate> birthDates;
+        private Button addBirthdateButton;
+        private Button calculateSubscriptionButton;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CurrentDateInitialize();
+            BirthDatesInitialize();
+            AddBirthdateButtonInitialize();
+            CalculateSubscriptionInitialize();
+            ResetPositions();
+        }
+
+        private void CurrentDateInitialize()
+        {
+            currentDate = new Date(this, currentDateLabelText);
+        }
+        private void BirthDatesInitialize()
+        {
+            birthDates = new List<BirthDate>();
+        }
+        private void BirthDatesAdd()
+        {
+            birthDates.Add(new BirthDate(this));
+        }
+        internal void BirthDatesRemove(BirthDate toRemove)
+        {
+            birthDates.Remove(toRemove);
+        }
+        private void AddBirthdateButtonInitialize()
+        {
+            addBirthdateButton = new Button();
+            addBirthdateButton.Width = addBirthdateButtonWidth;
+            addBirthdateButton.Text = addBirthdateButtonText;
+            addBirthdateButton.Click += new EventHandler(ButtonFunctionAddBithdateButton);
+            Controls.Add(addBirthdateButton);
+        }
+        private void CalculateSubscriptionInitialize()
+        {
+            calculateSubscriptionButton = new Button();
+            calculateSubscriptionButton.Width = calculateSubscriptionButtonWidth;
+            calculateSubscriptionButton.Text = calculateSubscriptionButtonText;
+            calculateSubscriptionButton.Click += new EventHandler(ButtonFunctionCalculateSubscription);
+            Controls.Add(calculateSubscriptionButton);
+        }
+
+        internal void ResetPositions()
+        {
+            int rowNumber = 0;
+            currentDate.ChangePosition(widthMargin, heightMargin + rowHeight * rowNumber);
+            rowNumber++;
+            foreach(BirthDate birthDate in birthDates)
+            {
+                birthDate.ChangePosition(widthMargin, heightMargin + rowHeight * rowNumber);
+                rowNumber++;
+            }
+            addBirthdateButton.Location = new Point(widthMargin, heightMargin + rowHeight * rowNumber);
+            rowNumber++;
+            calculateSubscriptionButton.Location = new Point(widthMargin, heightMargin + rowHeight * rowNumber);
+        }
+
+        private void ButtonFunctionAddBithdateButton(object sender, EventArgs e)
+        {
+            BirthDatesAdd();
+            ResetPositions();
+        }
+        private void ButtonFunctionCalculateSubscription(object sender, EventArgs e)
+        {
+            int minAdultAge = CalculateSubscriptions.GetMinAdultAge();
+            int minElderlyAge = CalculateSubscriptions.GetMinElderlyAge();
+            int childArrayPosition = CalculateSubscriptions.GetChildArrayPosition();
+            int adultArrayPosition = CalculateSubscriptions.GetAdultArrayPosition();
+            int elderlyArrayPosition = CalculateSubscriptions.GetElderlyArrayPosition();
+
+            int yearIndex = Date.GetYearIndex();
+            int monthIndex = Date.GetMonthIndex();
+            int dayIndex = Date.GetDayIndex();
+
+            int[] peopleCount = new int[CalculateSubscriptions.GetArraySize()];
+            for(int i = 0;i< peopleCount.Length; i++)
+            {
+                peopleCount[i] = 0;
+            }
+
+            int[] compareToDate = currentDate.GetDate();
+
+            foreach(BirthDate birthDate in birthDates)
+            {
+                int[] date = birthDate.GetDate();
+                if (date[yearIndex] + minElderlyAge < compareToDate[yearIndex] || (date[yearIndex] + minElderlyAge <= compareToDate[yearIndex] && date[monthIndex] < compareToDate[monthIndex]) || (date[yearIndex] + minElderlyAge <= compareToDate[yearIndex] && date[monthIndex] <= compareToDate[monthIndex] && date[dayIndex] <= compareToDate[dayIndex]))
+                {
+                    peopleCount[elderlyArrayPosition]++;
+                }
+                else if (date[yearIndex] + minAdultAge < compareToDate[yearIndex] || (date[yearIndex] + minAdultAge <= compareToDate[yearIndex] && date[monthIndex] < compareToDate[monthIndex]) || (date[yearIndex] + minAdultAge <= compareToDate[yearIndex] && date[monthIndex] <= compareToDate[monthIndex] && date[dayIndex] <= compareToDate[dayIndex]))
+                {
+                    peopleCount[adultArrayPosition]++;
+                }
+                else
+                {
+                    peopleCount[childArrayPosition]++;
+                }
+            }
+            MessageBox.Show(CalculateSubscriptions.GetSubscriptionsString(peopleCount));
+        }
+
+        internal class Date
+        {
+            protected const int textBoxOfset = 100;
+            protected const int textBoxBetweenOfset = 30;
+            protected const int textBoxWidth = 25;
+
+            protected const int dateArrayLength = 3;
+            protected const int dateDayIndex = 0;
+            protected const int dateMonthIndex = 1;
+            protected const int dateYearIndex = 2;
+
+            protected Label label;
+            protected TextBox textBoxDay;
+            protected TextBox textBoxMonth;
+            protected TextBox textBoxYear;
+
+            protected Form1 form;
+
+            internal Date(Form1 form, string labelText)
+            {
+                this.form = form;
+
+                label = new Label();
+                label.Text = labelText;
+                form.Controls.Add(label);
+
+                textBoxDay = new TextBox();
+                textBoxDay.Width = textBoxWidth;
+                form.Controls.Add(textBoxDay);
+
+                textBoxMonth = new TextBox();
+                textBoxMonth.Width = textBoxWidth;
+                form.Controls.Add(textBoxMonth);
+
+                textBoxYear = new TextBox();
+                textBoxYear.Width = textBoxWidth;
+                form.Controls.Add(textBoxYear);
+            }
+
+            internal void ChangePosition(int widthOfset, int heightOfset)
+            {
+                CorrectPosition(widthOfset, heightOfset);
+            }
+            protected void CorrectPosition(int widthOfset, int heightOfset)
+            {
+                label.Location = new Point(widthOfset, heightOfset);
+                textBoxDay.Location = new Point(widthOfset + textBoxOfset, heightOfset);
+                textBoxMonth.Location = new Point(widthOfset + textBoxOfset + textBoxBetweenOfset, heightOfset);
+                textBoxYear.Location = new Point(widthOfset + textBoxOfset + textBoxBetweenOfset * 2, heightOfset);
+            }
+            internal int[] GetDate()
+            {
+                int[] date = new int[dateArrayLength];
+                date[dateDayIndex] = int.Parse(textBoxDay.Text);
+                date[dateMonthIndex] = int.Parse(textBoxMonth.Text);
+                date[dateYearIndex] = int.Parse(textBoxYear.Text);
+                return date;
+            }
+            internal static int GetYearIndex()
+            {
+                return dateYearIndex;
+            }
+            internal static int GetMonthIndex()
+            {
+                return dateMonthIndex;
+            }
+            internal static int GetDayIndex()
+            {
+                return dateDayIndex;
+            }
+        }
+        internal class BirthDate : Date
+        {
+            private const string labelText = "date of birth (day-month-year):";
+            private const string buttonText = "remove";
+
+            private Button button;
+
+            internal BirthDate(Form1 form) : base(form, labelText)
+            {
+                label.Text = labelText;
+
+                button = new Button();
+                button.Text = buttonText;
+                button.Click += new EventHandler(ButtonFunction);
+                form.Controls.Add(button);
+            }
+            private void ButtonFunction(object sender, EventArgs e)
+            {
+                form.Controls.Remove(label);
+                form.Controls.Remove(textBoxDay);
+                form.Controls.Remove(textBoxMonth);
+                form.Controls.Remove(textBoxYear);
+                form.Controls.Remove(button);
+                form.BirthDatesRemove(this);
+                form.ResetPositions();
+            }
+            internal void ChangePosition(int widthOfset, int heightOfset)
+            {
+                CorrectPosition(widthOfset, heightOfset);
+                button.Location = new Point(widthOfset + textBoxOfset + textBoxBetweenOfset * 3, heightOfset);
+            }
+        }
+    }
+}
